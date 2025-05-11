713e92c2a7144348b0ff0f511bdd3230d47a3523
Fusingsign254111 <92048485+Fusingsign254111@users.noreply.github.com>
2025-05-11 22:49:33 +0100

    Update source.lua

--- source.lua
+++ source.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet('https://raw.githubusercontent.com/Fusingsign254111/Slave-hub/refs/heads/main/SlavehubV2.lua?token=GHSAT0AAAAAADCRU6ZY4TKCNGCK5DC54MKA2BBDLXA'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Fusingsign254111/Slave-hub/refs/heads/main/SlavehubV2.lua?token=GHSAT0AAAAAADCRU6ZYD67NXELE5A7ROOGA2BBDOPA'))()
